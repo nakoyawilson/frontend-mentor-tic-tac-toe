8a04be47640f4b1c8baf223399151582458965eb
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-05-12 18:55:08 -0400

    Add starter files for tic-tac-toe project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!-- New game menu start --> */}
+      Pick player 1's mark Remember: X goes first New Game (vs CPU) New Game (vs
+      player)
+      {/* <!-- New game menu end --> */}
+      {/* <!-- Game board start --> */}
+      {/* <!-- x/o icon -->  */}turn X (You) {/* <!-- Your score --> */}
+      Ties {/* <!-- Ties score --> */}X (CPU) {/* <!-- CPU score --> */}
+      Oh no, you lost You won! Player {/* <!-- 1/2 --> */} wins!
+      {/* <!-- x/o icon --> */} takes the round Round tied Restart game? Quit
+      Next round No, cancel Yes, restart
+      {/* <!-- Game board end --> */}
     </div>
   );
 }
