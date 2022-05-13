1faa7391f0e642581947f40c66a5f569769a5bf9
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-05-12 22:36:01 -0400

    Make minor adjustments to mobile view of new game modal

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ const App = () => {
   const [showResultsModal, setShowResultsModal] = useState(false);
   const [playAgainstCPU, setPlayAgainstCPU] = useState(null);
 
+  const startGame = (e) => {
+    if (e.target.value === "cpu") {
+      setPlayAgainstCPU(true);
+    } else if (e.target.value === "human") {
+      setPlayAgainstCPU(false);
+    }
+    setShowNewGameModal(false);
+  };
+
   return (
     <>
       <h1 className="visually-hidden">Tic Tac Toe</h1>
@@ -18,7 +27,7 @@ const App = () => {
           <div className="new-game container">
             <img src={logo} alt="Tic-tac-toe XO logo" className="logo" />
             <form className="icon-settings">
-              <h2 className="heading">Pick player 1's mark</h2>
+              <h2 className="heading">Pick player 1&rsquo;s mark</h2>
               <div className="icons-wrapper">
                 <input
                   type="radio"
@@ -29,14 +38,16 @@ const App = () => {
                 <label htmlFor="iconX" className="icon-label">
                   <span className="visually-hidden">X icon</span>
                   <svg
-                    width="64"
-                    height="64"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    class="icon-wrapper"
                   >
                     <path
-                      d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"
-                      fillRule="evenodd"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M31.5569 5.28973L26.7103 0.443061C26.1195 -0.147687 25.1617 -0.147687 24.571 0.443061L16 9.01404L7.42902 0.443061C6.83827 -0.147687 5.88048 -0.147687 5.28973 0.443061L0.443061 5.28973C-0.147687 5.88048 -0.147687 6.83827 0.443061 7.42902L9.01404 16L0.443061 24.571C-0.147687 25.1617 -0.147687 26.1195 0.443061 26.7103L5.28973 31.5569C5.88048 32.1477 6.83827 32.1477 7.42902 31.5569L16 22.986L24.571 31.5569C25.1617 32.1477 26.1195 32.1477 26.7103 31.5569L31.5569 26.7103C32.1477 26.1195 32.1477 25.1617 31.5569 24.571L22.986 16L31.5569 7.42902C32.1477 6.83827 32.1477 5.88048 31.5569 5.28973Z"
                       className="icon"
                     />
                   </svg>
@@ -46,18 +57,20 @@ const App = () => {
                   name="playerIcon"
                   id="iconO"
                   className="visually-hidden icon-option"
-                  defaultChecked
                 />
                 <label htmlFor="iconO" className="icon-label">
                   <span className="visually-hidden">O icon</span>
                   <svg
-                    width="64"
-                    height="64"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    class="icon-wrapper"
                   >
                     <path
-                      d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M31.7412 15.8706C31.7412 7.10551 24.6357 0 15.8706 0C7.10551 0 0 7.10551 0 15.8706C0 24.6357 7.10551 31.7412 15.8706 31.7412C24.6357 31.7412 31.7412 24.6357 31.7412 15.8706ZM9.4048 15.8706C9.4048 12.2996 12.2996 9.4048 15.8706 9.4048C19.4416 9.4048 22.3364 12.2996 22.3364 15.8706C22.3364 19.4416 19.4416 22.3364 15.8706 22.3364C12.2996 22.3364 9.4048 19.4416 9.4048 15.8706Z"
                       className="icon"
                     />
                   </svg>
@@ -65,29 +78,23 @@ const App = () => {
               </div>
               <p class="paragraph">Remember: X goes first</p>
             </form>
-            <form>
-              <input
-                type="radio"
-                name=""
-                id="cpuPlayer"
-                className="visually-hidden"
-              />
-              <label htmlFor="cpuPlayer" className="new-game-btn new-game-cpu">
-                New Game (vs CPU)
-              </label>
-              <input
-                type="radio"
-                name=""
-                id="humanPlayer"
-                className="visually-hidden"
-              />
-              <label
-                htmlFor="humanPlayer"
-                className="new-game-btn new-game-human"
-              >
-                New Game (vs player)
-              </label>
-            </form>
+            <button
+              className="new-game-btn new-game-cpu"
+              type="button"
+              value="cpu"
+              onClick={startGame}
+            >
+              New Game (vs CPU)
+            </button>
+            <button
+              htmlFor="humanPlayer"
+              className="new-game-btn new-game-human"
+              type="button"
+              value="human"
+              onClick={startGame}
+            >
+              New Game (vs player)
+            </button>
           </div>
         </Modal>
       )}
